8deae83d48c564596520f81d6ec521a87aeb247f
Andre Borges <andreborges1777@gmail.com>
2020-06-24 19:41:03 -0300

    Adcionando Commets aos Posts

--- src/main/java/com/workshop/workshopmongo/domain/Post.java
+++ src/main/java/com/workshop/workshopmongo/domain/Post.java
@@ -1,18 +1,19 @@
 package com.workshop.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import com.workshop.workshopmongo.dto.AuthorDTO;
+import com.workshop.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode
 @Document
 public class Post implements Serializable{
@@ -24,5 +25,15 @@ public class Post implements Serializable{
     private String title;
     private String body;
     private AuthorDTO author;
-        
+
+    private List<CommentDTO> comments = new ArrayList<>();
+
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
+    
 }
