5e71a46197f696c43ac57a3d8a8c358c1638d853
Andre Borges <andreborges1777@gmail.com>
2020-06-24 22:53:51 -0300

    Consulta com vários critérios

--- src/main/java/com/workshop/workshopmongo/resources/util/URL.java
+++ src/main/java/com/workshop/workshopmongo/resources/util/URL.java
@@ -1,6 +1,9 @@
 package com.workshop.workshopmongo.resources.util;
 
 import java.net.URLDecoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
     
@@ -9,6 +12,16 @@ public class URL {
             return URLDecoder.decode(text, "UTF-8");
         } catch (Exception e) {
             return "";
+        }        
+    }
+
+    public static Date convertDate(String textDate, Date defaultValue){
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+            return sdf.parse(textDate);
+        } catch (Exception e) {
+            return defaultValue;
         }
         
     }
